80c71839b75fe5c8e98868d3ec7a98b097821884
ZaizafunHanifah <hanifah9158@gmail.com>
2024-12-09 19:26:20 +0700

    firts commit

---
+++ register.php
@@ -0,0 +1,49 @@
+<?php 
+
+include 'connect.php';
+
+if(isset($_POST['signUp'])){//nama tombol
+    $email = $_POST['email'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $password=md5($password);
+
+     $checkUsername="SELECT * From users where username='$username'";
+     $result=$conn->query($checkUsername);
+     if($result->num_rows>0){
+        echo "Username Already Exists !";
+     }
+     else{
+        $insertQuery="INSERT INTO users(username,email,password)
+                       VALUES ('$username','$email','$password')";
+            if($conn->query($insertQuery)==TRUE){
+                header("location: index.php"); //lokasi halaman
+            }
+            else{
+                echo "Error:".$conn->error;
+            }
+     }
+   
+
+}
+
+if(isset($_POST['signIn'])){
+   $username=$_POST['username'];
+   $password=$_POST['password'];
+   $password=md5($password) ;
+   
+   $sql="SELECT * FROM users WHERE username='$username' and password='$password'";
+   $result=$conn->query($sql);
+   if($result->num_rows>0){
+    session_start();
+    $row=$result->fetch_assoc();
+    $_SESSION['username']=$row['username'];
+    header("Location: homepage.php"); //lokasi halaman home
+    exit();
+   }
+   else{
+    echo "Not Found, Incorrect Email or Password";
+   }
+
+}
+?>
