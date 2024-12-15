9e2ddfa22bd4a81a3dee4a299e64dcfd78c63c48
keroppiii <fatimah.nurmawati@mhs.unsoed.ac.id>
2024-12-15 12:35:58 +0700

    news++

---
+++ news/news.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Berita</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../assets/css/homepage.css">
+    <link rel="stylesheet" href="../assets/css/news.css">
+    <link href="https://fonts.googleapis.com/css2?family=Bodoni+Moda:ital,opsz,wght@0,6..96,400..900;1,6..96,400..900&family=Cardo:ital,wght@0,400;0,700;1,400&family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+</head>
+<body>
+<header>
+    <div class="navbar">
+        <div class="logo">
+            <img src="../assets/images/logo-liverpool.png" alt="Liverpool FC Logo">
+            <span class="logo-text">Liverpool</span>
+        </div>
+        <nav class="menu">
+            <a href="../homepage/index.php">Home</a>
+            <a href="../players/players.php">Players</a>
+            <a href="../merchandise/merch.php">Merchandise</a>
+            <a href="../news/news.php">News</a>
+        </nav>
+        
+        <div class="auth-links">
+          <a href="../login/login-user.html" class="login">Log in</a>
+          <a href="../signup/signup-user.html" class="signup">Sign Up</a>
+        </div>
+    </div>
+  </header>
+
+  <div class="search-bar">
+    <i data-feather="search"></i>
+    <input type="text" placeholder="What Are You Looking For?">
+  </div>
+
+  <div class="judul4">
+          <h1>What's Goin On About Liverpool</h1>
+        </div>
+      <section class="news-section">
+        <div class="news-container">
+          <!-- News Card 1 -->
+          <div class="news-card red">
+            <img src="../assets/images/homepage/news/1.webp" alt="Alexis Mac Allister">
+            <h3>Alexis Mac Allister Akan Dihentikan Penangguhan Liga Champions</h3>
+            <a href="https://www.liverpoolfc.com/id/news/alexis-mac-allister-serve-champions-league-suspension" target="_blank" class="view-more">View More →</a>
+          </div>
+          <!-- News Card 2 -->
+          <div class="news-card red">
+            <img src="../assets/images/homepage/news/2.webp" alt="Liverpool vs Real Madrid">
+            <h3>Liverpool 2-0 Real Madrid: Tonton sorotan dan 90 menit penuh</h3>
+            <a href="https://www.liverpoolfc.com/id/news/liverpool-2-0-real-madrid-watch-highlights-and-full-90-minutes" target="_blank" class="view-more">View More →</a>
+          </div>
+          <!-- News Card 3 -->
+          <div class="news-card red">
+            <img src="../assets/images/homepage/news/3.webp" alt="Andy Robertson">
+            <h3>Andy Robertson mengulas Real Madrid dan pratinjau Manchester City</h3>
+            <a href="https://www.liverpoolfc.com/id/news/rest-recover-repeat-andy-robertson-reviews-real-madrid-and-previews-manchester-city" target="_blank" class="view-more">View More →</a>
+          </div> 
+
+            <!-- News Card 4 -->
+          <div class="news-card red">
+            <img src="../assets/images/news/u18news.webp" alt="U-18">
+            <h3>U18s match report: Liverpool close out 2024 with 4-2 win at home to Wolves</h3>
+            <a href="https://www.liverpoolfc.com/news/u18s-match-report-liverpool-close-out-2024-4-2-win-home-wolves" target="_blank" class="view-more">View More →</a>
+          </div>
+          <!-- News Card 5 -->
+          <div class="news-card red">
+            <img src="../assets/images/news/virgil.webp" alt="Virgil">
+            <h3>Virgil van Dijk: Bisa jadi lebih — tapi kami akan mengambil intinya</h3>
+            <a href="https://www.liverpoolfc.com/id/news/virgil-van-dijk-it-could-have-been-more-well-take-point" target="_blank" class="view-more">View More →</a>
+          </div>
+          
+            <!-- News Card 6 -->
+            <div class="news-card red">
+            <img src="../assets/images/news/fulham.webp" alt="Fulham">
+            <h3>Konferensi pers Arne Slot yang bangga dengan karakter tim : Liverpool 2-2 Fulham</h3>
+            <a href="https://www.liverpoolfc.com/id/news/arne-slot-press-conference-liverpool-2-2-fulham" target="_blank" class="view-more">View More →</a>
+          </div>
+          <!-- News Card 7 -->
+          <div class="news-card red">
+            <img src="../assets/images/news/mohSalah.webp" alt="Moh Salah">
+            <h3>Mohamed Salah Catat Assist ke-100 untuk Liverpool selama imbang 2 - 2</h3>
+            <a href="https://www.liverpoolfc.com/id/news/mohamed-salah-records-100th-assist-liverpool" target="_blank" class="view-more">View More →</a>
+          </div>
+
+          <!-- News Card 8 -->
+          <div class="news-card red">
+            <img src="../assets/images/news/niamhFahey.webp" alt="Niamh Fahey">
+            <h3>Niamh Fahey: Mari kita coba mengakhiri 2024 dengan level tertinggi melawan Arsenal</h3>
+            <a href="https://www.liverpoolfc.com/id/news/niamh-fahey-lets-try-end-2024-real-high-against-arsenal" target="_blank" class="view-more">View More →</a>
+          </div>
+
+          <!-- News Card 9 -->
+          <div class="news-card red">
+            <img src="../assets/images/news/arneSlot.webp" alt="Arne Slot">
+            <h3>Arne Slot: Upaya kolektif membawa kami ke sini dan harus dipertahankan</h3>
+            <a href="https://www.liverpoolfc.com/id/news/arne-slot-collective-effort-has-brought-us-here-and-we-have-maintain-it" target="_blank" class="view-more">View More →</a>
+          </div>
+
+          <!-- News Card 10 -->
+          <div class="news-card red">
+            <img src="../assets/images/news/WSL.webp" alt="WSL">
+            <h3>Statistik Pra-Arsenal: Liverpool Siap Membawa Tonggak WSL pada Minggu</h3>
+            <a href="https://www.liverpoolfc.com/id/news/pre-arsenal-stats-liverpool-set-bring-wsl-milestone-sunday" target="_blank" class="view-more">View More →</a>
+          </div>
+
+          <!-- News Card 11 -->
+          <div class="news-card red">
+            <img src="../assets/images/news/LFC-Wanita.webp" alt="LFC Wanita">
+            <h3>Informasi Hari Pertandingan Penting : LFC Wanita menjamu Everton di Stadion St Helens</h3>
+            <a href="https://www.liverpoolfc.com/id/news/lfc-women-v-arsenal-important-matchday-information" target="_blank" class="view-more">View More →</a>
+          </div>
+
+          <!-- News Card 12 -->
+          <div class="news-card red">
+            <img src="../assets/images/news/Alexander.webp" alt="Alexander">
+            <h3>Trent Alexander-Arnold tentang keyakinan Liverpool dan 'kesenian' kelompokan</h3>
+            <a href="https://www.liverpoolfc.com/id/news/trent-alexander-arnold-liverpools-belief-and-artistry-passing" target="_blank" class="view-more">View More →</a>
+          </div>
+        </div>
+      </section>
+
+
+
+
+
+
+
+  <footer class="footer">
+  <div class="social-icons">
+    <a href="https://www.facebook.com/" target="_blank"><img src="https://cdn.simpleicons.org/facebook/FFFFFF" alt="Facebook"></a>
+    <a href="https://twitter.com/" target="_blank">
+    <img src="https://cdn.simpleicons.org/x/FFFFFF" alt="X (Twitter)">
+    </a>
+
+    <a href="https://www.instagram.com/" target="_blank"><img src="https://cdn.simpleicons.org/instagram/FFFFFF" alt="Instagram"></a>
+    <a href="https://www.tiktok.com/" target="_blank"><img src="https://cdn.simpleicons.org/tiktok/FFFFFF" alt="TikTok"></a>
+    <a href="https://www.youtube.com/" target="_blank"><img src="https://cdn.simpleicons.org/youtube/FFFFFF" alt="YouTube"></a>
+  </div>
+
+    <p>DOWNLOAD THE OFFICIAL LFC APP</p>
+  <div class="app-buttons">
+    <a href="https://www.apple.com/app-store/" target="_blank">
+      <img src="https://developer.apple.com/assets/elements/badges/download-on-the-app-store.svg" alt="Download on the App Store">
+    </a>
+    <a href="https://play.google.com/store" target="_blank">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/7/78/Google_Play_Store_badge_EN.svg" alt="Get it on Google Play">
+    </a>
+  </div>
+  <p>© Copyright 2024 All rights reserved.</p>
+</footer>
+
+  <script src="https://unpkg.com/feather-icons"></script>
+  <script>
+    feather.replace(); // Mengaktifkan Feather Icons
+  </script>
+  <script src="../assets/js/script.js"></script>
+</body>
+</html>
